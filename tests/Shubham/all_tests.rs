87613d10f8690d85641f3fcab4de765e60806ae3
Shubham Bhargava <shubhargava@gmail.com>
2023-05-22 10:13:32 -0700

    added test case

---
+++ tests/Shubham/all_tests.rs
@@ -0,0 +1,18 @@
+mod infra;
+
+success_tests! {
+    {
+        name: twenty_one,
+        file: "twenty_one.snek",
+        expected: "19",
+    },
+}
+
+runtime_error_tests! {
+
+}
+
+static_error_tests! {
+
+
+}
